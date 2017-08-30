acd62d55ad6acfc8d9c0b7afd53ee006767f2e9e
saluki-scribe <tara@nelsonwriting.com>
2017-08-29 20:47:10 -0400

    Figured out a good way to organize quiz questions. Successfully printed to the screen.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,83 @@
 $(document).ready(function(){
 
+ /*   var question = new Object();
+
+    question.number = "1";
+    question.question = "La";
+    question.rightAnswer = "d";
+    question.wrongAnswers = ["a", "b", "c"];
+
+    function Question(question, rightAnswer, wrongAnswers, rightGif, wrongGif)
+        this.question = question;
+        this.rightAnswer = rightAnswer;
+        this.wrongAnswers = wrongAnswers[];
+        this.rightGif = rightGif;
+        this.wrongGif = wrongGif;
+
+var q1 = new Question("What's the smartest way to monologue to the hero?",
+        "c) Don't. Say it to your rubber duck.", "", "");
+
+var q2 = new Question("");*/
+
+var Q1 = {
+q: "How are you?",
+a1: "A) Super",
+a2: "B) Good",
+a3: "C) All right",
+a4: "D) Crummy",
+rightAnswer: true,
+}
+
+var Q2 = {
+    q: "Are you sure?",
+    a1: "A) Yes",
+    a2: "B) I think so...",
+    a3: "C) No. I told you before.",
+    a4: "D) Why are you doing this to me?",
+    rightAnswer: true,
+}
+
+$("#content-div p").html(Q1.q + "<br>" + Q1.a1 + "<br>" + Q1.a2 + "<br>" + Q1.a3 + "<br>" + Q1.a4);
+
+
+if () {
+    
+}
+
+
+    //Create a trivia game object
+    //Shows only one question and four choices
+    //Shows time remaining
+    //Question shows until the player answers it or time runs out
+
+    //If player selects correct answer:
+    //Show a screen congratulating them. 
+    //After a few seconds, display the next question (without user input)
+
+    //If player selects wrong answer:
+    //Tell them they selected wrong option and display correct answer.
+    //Wait a few seconds, then show the next question
+
+    //If runs out of time:
+    //Tell them that time's up and display the correct answer.
+    //Wait a few seconds, then show the next question
+
+    //On final screen:
+    //Show number of correct answers, incorrect answers, and an option to restart the game (without reloading the page)
+
 
 
 })
+
+
+
+/*
+The Fantasy Villain's Trope Primer
+
+1) 
+2)
+3)
+4)
+5)
+
+*/
